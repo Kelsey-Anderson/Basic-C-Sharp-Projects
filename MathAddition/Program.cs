d8cb6bdd794bed76d9b0331c059115e6316c9e66
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-07-28 18:05:18 -0700

    Adding to Repository

---
+++ MathAddition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+
+            int num1 = 5;
+            int num2 = 7;
+            int difference = num1 - num2;
+            Console.WriteLine(difference);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ VariablesAndDataTyles/VariablesAndDataTyles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace VariablesAndDataTyles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -9;
+            //char randomLetter = "X";
+            //char questionMark = '\u2103';
+            //decimal moneyInBank = 100.5m;
+            //double heightInCentimeters = 211.30202092;
+            //float secondsLeft = 2.62f;
+            //short temperatureonmars = -341;
+            //string myName = "kelsey";
+
+            //int CurrentAge = 30;
+            //string yearsOld = CurrentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+        
+
+
+
+        }
+    }
+}
