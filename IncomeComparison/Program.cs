658da3f1372eaab3d8ce0c270537e3c9d795c1a0
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-07-29 19:09:18 -0700

    Upload

---
+++ IncomeComparison/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            string Person1;
+            Console.WriteLine("Person 1 \n Hourly Rate?");
+            int HrRate1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int totalHours1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            string Person2;
+            Console.WriteLine("Person 2 \n  Hourly Rate?");
+            int HrRate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int totalHours2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Annual salary of Person 1:");
+            int perWeek1 = HrRate1 * totalHours1;
+            int annualSal1 = 52 * perWeek1;
+            Console.WriteLine(annualSal1);
+            Console.ReadLine();
+
+            Console.WriteLine("Annual salary of Person 2:");
+            int perWeek2 = HrRate2 * totalHours2;
+            int annualSal2 = 52 * perWeek2;
+            Console.WriteLine(annualSal2);
+            Console.ReadLine();
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool trueORfalse = annualSal1 > annualSal2;
+            Console.WriteLine(trueORfalse.ToString());
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
--- MathAddition/Program.cs
+++ MathAddition/Program.cs
@@ -6,13 +6,105 @@ namespace MathAddition
     {
         static void Main(string[] args)
         {
-     
+            //int num1 = 5;
+            //int num2 = 7;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
 
-            int num1 = 5;
-            int num2 = 7;
-            int difference = num1 - num2;
-            Console.WriteLine(difference);
-            Console.ReadLine();
+            //int num1 = 5;
+            //int num2 = 7;
+            //int difference = num1 - num2;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 7;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+
+            //int num1 = 12;
+            //int num2 = 3;
+            //int quotient = num1 / num2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int num1 = 7;
+            //int num2 = 3;
+            //int remainder = num1 % num2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+
+            //int number = 8;
+            //int remainder = number % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+
+            //string firstName = "Kelsey";
+            //string lastName = "Anderson";
+            //Console.WriteLine(firstName + lastName);
+            //Console.ReadLine();
+
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus five = " + difference.ToString());
+            //Console.ReadLine();
+
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.Write(remainder);
+            //Console.ReadLine();
+
+
+            //int num1 = 200 / 3;
+            //Console.WriteLine(num1);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine(); //should output True
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine(); //should output False
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            ////bool isWarm = currentTemperature >= roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
+
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+
+
 
 
 
