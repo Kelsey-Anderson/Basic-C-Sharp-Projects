b11a6ef6d3c24ee2531978844a196a89225cdb06
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-07-31 16:31:44 -0700

    Update

--- MathAddition/Program.cs
+++ MathAddition/Program.cs
@@ -103,13 +103,93 @@ namespace MathAddition
             //Console.ReadLine();
 
 
-            int age = 31;
-            bool hasTicket = true;
-            bool hasPassport = true;
+            //int age = 31;
+            //bool hasTicket = true;
+            //bool hasPassport = true;
 
-            bool canFly = (age >= 18 && hasTicket && hasPassport);
-            Console.WriteLine(canFly);
-            Console.ReadLine();
+            //bool canFly = (age >= 18 && hasTicket && hasPassport);
+            //Console.WriteLine(canFly);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your name?");
+
+            //string name = Console.ReadLine();
+            //if (name == "Jesse")
+            //{
+            //    Console.WriteLine("Your name is Jesse");
+            //}
+            //else if (name == "Brett")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Jesse OR Brett.");
+            //}
+
+
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //if (name != "Jesse")
+            //{
+            //    Console.WriteLine("Your name is not Jesse");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.Write("Hi, Jesse!");
+            //    Console.ReadLine();
+            //}
+
+
+            //int num1 = 12;
+            //int num2 = 14;
+            //if (num1 < num2)
+            //{
+            //    Console.WriteLine("The first number is larger.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The second number is larger.");
+            //}
+
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter your age:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //if (age <= 20)
+            //{
+            //    Console.WriteLine("No you may not purchase that bottle of wine.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You're of age, here's your wine!");
+            //}
+            //Console.ReadLine();
+
+            //int num1 = 7;
+            //int num2 = 12;
+            //int num3 = 2;
+            //int num4 = 8;
+
+            //if (num1 <= num2 && num4 >= num3)
+            //{
+            //    Console.WriteLine("the if statement ran.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The else statement ran.");
+            //}
+            //Console.ReadLine();
+
+
+            //int num1 = 2;
+            //int num2 = 12;
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
 
 
 
---
+++ ShippingQuote/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+
+            Console.WriteLine("Package weight?");
+            int pkgWeight = Convert.ToInt32(Console.ReadLine()); //user input string converts to int
+            if (pkgWeight > 50) //pkg must be less than 50 or program will end.
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return; // ends the program if "true" 
+            }
+
+            //if weight is less than 50, program will continue:
+
+            Console.WriteLine("Package width?");
+            int pkgWidth = Convert.ToInt32(Console.ReadLine());//user input string converts to int
+
+            Console.WriteLine("Package height?");
+            int pkgHeight = Convert.ToInt32(Console.ReadLine());//user input string converts to int
+
+            Console.WriteLine("Package length?");
+            int pkgLength = Convert.ToInt32(Console.ReadLine());//user input string converts to int
+
+            int totalDims = pkgWidth + pkgHeight + pkgLength; 
+
+            if (totalDims > 50) //pkg dims must be less than 50 or program will end.
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return; // ends the program if "true"
+            }
+            else //pkg dims are less than 50
+            {
+
+                int productDims = (pkgWidth * pkgHeight * pkgLength) * pkgWeight; //does the math
+                int quote = productDims / 100; //estimates quote amount
+
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote + " \n Thank You!");
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
