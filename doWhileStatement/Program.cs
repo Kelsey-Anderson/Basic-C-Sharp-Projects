8af9f5abe433945236f68368da27db7828645ae6
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-08-01 15:03:16 -0700

    update

--- doWhileStatement/Program.cs
+++ doWhileStatement/Program.cs
@@ -7,40 +7,40 @@ namespace doWhileStatement
         static void Main(string[] args)
         {
             Console.WriteLine("Guess a number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = number == 12;
+            int number = Convert.ToInt32(Console.ReadLine()); //user inpurt string, converts to int
+            bool isGuessed = number == 12; // this is the secret number 
 
             do
             {
-                switch (number)
+                switch (number) // swtich statement
                 {
                     case 62:
                         Console.WriteLine("You guessed 62. Try again.");
-                        Console.WriteLine("Guess a number");
+                        Console.WriteLine("Guess a number"); // if user guessed this case, they will get another chance to guess again.
                         number = Convert.ToInt32(Console.ReadLine());
-                        break;
+                        break; 
                     case 29:
                         Console.WriteLine("You guessed 29. Try again.");
-                        Console.WriteLine("Guess a number");
+                        Console.WriteLine("Guess a number");// if user guessed this case, they will get another chance to guess again.
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 55:
                         Console.WriteLine("You guessed 55. Try again.");
-                        Console.WriteLine("Guess a number");
+                        Console.WriteLine("Guess a number");// if user guessed this case, they will get another chance to guess again.
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 12:
                         Console.WriteLine("You guessed the number 12. That is correct!");
-                        isGuessed = true;
+                        isGuessed = true; // changed the bool to true
                         break;
-                    default:
-                        Console.WriteLine("Your are wrong.");
-                        Console.WriteLine("Guess a number");
+                    default: // use if user input something that wasn't used in a case 
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number");// user will get another chance to guess again.
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
                 }
             }
-            while (!isGuessed);
+            while (!isGuessed); // run while the bool is false
             Console.ReadLine();
         }
     }
