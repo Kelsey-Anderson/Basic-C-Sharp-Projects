08598089b3bc1137df459f172b57a149683853c4
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-08-10 17:43:09 -0700

    update

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            Console.WriteLine("Pick a number");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another number");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two ....");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        catch (FormatException ex) //catches format exceptsion only ... "ex" is the variable datatype = exception
+        {
+            Console.WriteLine("Please type a whole number"); //writes error to the screen
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+           // return; 
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.Message);//general exception .. prints whatever system exception is.
+        }
+        finally // will run no matter what, exception or not
+        {
+            Console.ReadLine();
+        }
+           
+    }
+        
+    }
+
---
+++ TryCatchExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        try
+        {
+            List<int> IntList = new List<int>() { 20, 17, 109 };
+
+            Console.WriteLine("Type a number");
+            int num1 = Convert.ToInt32(Console.ReadLine()); // user input as string - convert to int
+
+            for (int x = 0; x < IntList.Count; x++) // for loop to iterate through list
+            {
+                var result = IntList[x] / num1; // math equation for each element of the list
+                Console.WriteLine(IntList[x] + " divided by " + num1 + " equals " + result);
+            }
+            Console.ReadLine();
+        }
+        catch (DivideByZeroException ex)
+        {
+            
+            Console.WriteLine(ex.Message); // system generated error message
+            Console.WriteLine("Please don't divide by zero"); // customer error message
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);// system generated error message
+            Console.WriteLine("Please only type a whole number.");// customer error message
+        }
+        finally
+        {
+            Console.ReadLine(); // this will run whether there was an exception or not.
+        }
+
+        }
+    }
+
