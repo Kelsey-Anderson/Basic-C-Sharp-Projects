3090eb9b4e8c2e6352c1ebfe33e27aa9db094d19
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-08-13 19:33:34 -0700

    update

--- MethodSubmission/MyClass.cs
+++ MethodSubmission/MyClass.cs
@@ -6,22 +6,12 @@ namespace MethodSubmission
 {
     public class MyClass
     {
-        public int AddingNums(int num1, string num2, int d)
+
+        public int AddingNums(int num1, int num2)
         {
-            int result1 = num1 + d;
-            int result2 = num1 + Convert.ToInt32(num2);
-
-            if (num2 == "")
-            {
-                return result1;
-            }
-            else
-            {
-                return result2;
-            }
-                
+            return num1 + num2; 
         }
-
+       
 
     }
 }
