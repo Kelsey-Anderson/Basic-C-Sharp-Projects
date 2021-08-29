1c243bcbdffd11ad8e1ed59f10299a920ac7afcc
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-08-28 18:40:39 -0700

    update

---
+++ LambdaPractice/Employees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaPractice
+{
+    public struct Employees // made this a struct that holds related data of various data types...employee record
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ID { get; set; }
+
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -50,7 +50,36 @@ namespace TwentyOne
             //int underlyingValue = (int)Suit.Diamonds; // casted suit.diamonds to an integer //could also use Convert.ToInt32(Suit.Diamonds);
             //Console.WriteLine(underlyingValue);
 
+            //Card card1 = new Card();
+            //Card card2 = card1; // reference type. Assigning value of card1 to card2, did NOT create a new card. Rather, we gave card2 the address to memory location of card1 to refer to. (think of google doc, one single central doc)
+            //card1.Face = Face.Eight;//if changing the CARD CLASS to be STRUCT instead, this will show Eight (card1 and card 2 are own separate instances)
+            //card2.Face = Face.King;//Leaving the CARD CLASS as it is will show King
+
+            //Console.WriteLine(card1.Face);
+
+            
+
+            //*List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList(); 
+            //Take list of cards and find out where the Face = king and map it to a NEW list.
+            //Where() is a lambda function, filter lists
+            //foreach (Card card in newList)
+            //{
+            //    Console.WriteLine(card.Face); // prints all items in the new list
+            //}
+
+
+            //another example
+            //List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 23 };
+            //int sum = numberList.Sum(); // can add add'l math inside Sum(x => x + 5); adds 5 to each item in list
+            //Console.WriteLine(sum);
+
+
+
+            // int count = deck.Cards.Count(x => x.Face == Face.Ace); 
+            // Cards is a list, Count() is a lambda function. Counting the elements in the list (x) where = Ace
+
             Deck deck = new Deck();  // creating an object "deck" of deck
+
             deck.Shuffle(3); // callling the method of shuffle
 
             foreach (Card card in deck.Cards)
