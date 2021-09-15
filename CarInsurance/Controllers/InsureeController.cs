2f0f3503627b4c36ba607f2f56a9ffda5224b3af
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-09-14 18:48:49 -0700

    update

---
+++ CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Quote()
+        {
+            int baseQuote = 50;
+
+            if ((DateTime.Now - DateOfBirth) < 18) 
+	            { baseQuote = baseQuote + 100; }
+            if ((DateTime.Now - DateOfBirth) >  19) || (DateTime.Now - DateOfBirth) < 25)
+	            { baseQuote = baseQuote + 50; }
+            if (DateOfBirth > 25)
+	            { baseQuote = baseQuote + 25; }
+
+
+            if (CarYear < 2000)
+                { baseQuote = baseQuote + 25; }
+            if (CarYear > 2000)
+                { baseQuote = baseQuote + 25; }
+
+
+            if (CarMake == "Porsche")
+                { baseQuote = baseQuote + 25; }
+            if (CarModel == "911 Carrera")
+                { baseQuote = baseQuote + 25; }
+
+            int tickets = SpeedingTickets * 10;
+
+            baseQuote = baseQuote + tickets;
+
+            if (DUI == true)
+                { baseQuote = baseQuote * 1.25; }
+
+
+            if (CoverageType == true)
+                { baseQuote = baseQuote * 1.50; }
+
+          
+
+        }
+
+
+    }
+}
---
+++ TryCatchAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+
+            if (userAge < 0)
+            {
+                throw new Exception();
+            }
+
+            while (userAge > 0)
+            {
+                try
+                {
+                    int yearBorn = current.Year - userAge;
+                    Console.WriteLine("You were born in {0}", yearBorn);
+                   
+                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error occured. Enter your age as whole number (and not a negative).");
+                    
+                    
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+            }
+
+            
+           
+        }
+    }
+}
