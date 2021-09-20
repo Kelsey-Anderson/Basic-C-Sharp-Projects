8f7afd43debfff910cb67fb5d2f16153712c27de
Kelsey-Anderson <84823575+Kelsey-Anderson@users.noreply.github.com>
2021-09-19 17:52:35 -0700

    update

---
+++ CodeFirstStudentDb/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstStudentDb
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public Grade Grade { get; set; }
+
+    }
+}
